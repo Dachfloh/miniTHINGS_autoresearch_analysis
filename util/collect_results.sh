2b5bd7993ab3984292f3bc76a9a18e33232cce8b
glados <glados@klab-1.hpc>
2026-09-03 12:21:37 +0200

    add more runs as well as util scripts

---
+++ util/collect_results.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter run tag: " RUN_TAG
+
+for((i=1;i<=7;i++)); do
+	echo $i
+	mkdir "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch_analysis/past_runs/agent$i/$RUN_TAG"
+	mv "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent$i/results.tsv" "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch_analysis/past_runs/agent$i/$RUN_TAG"
+done
---
+++ util/show_status.py
@@ -0,0 +1,21 @@
+import csv
+
+files = [
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent1/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent2/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent3/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent4/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent5/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent6/results.tsv",
+  "/home/staff/g/glados/autoresearch/miniTHINGS_autoresearch-agent7/results.tsv",
+]
+
+for filepath in files:
+  with open(filepath) as f:
+    print(filepath, '\nExperiments completed: ', (sum(1 for _ in f) - 2))
+
+  with open(filepath, newline='') as f:
+    reader = csv.DictReader(f, delimiter='\t')
+    max_value = max(float(row['test_acc']) for row in reader)
+
+  print(f"Max accuracy: {max_value}\n")
